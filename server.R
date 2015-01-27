dcdcb8a01787bdd04613a52b447fc80882c6cc41
teo <teofiln@gmail.com>
2015-01-26 22:52:18 -0600

    some code to switch between experiments

--- server.R
+++ server.R
@@ -3,41 +3,59 @@ library(ggplot2)
 library(gridExtra)
 library(plyr)
 library(scales)
+library(HH)
+
+# update the datasets 
+# creates symbolic links from original files in different folder
+# will work only localy
+system("./update_datasets.sh")
+
+# read all experiments: waller_salinity, waller_temperature, waller_flasks, cryptica_salinity
+Wsalt <- read.csv("./WSALT.csv", header=TRUE)
+Wtemp <- read.csv("./WTEMP.csv", header=TRUE)
+Wflas <- read.csv("./WFLAS.csv", header=TRUE)
+#Csalt <- read.csv("./CSALT.csv", header=TRUE)
+
+DAT <- rbind(Wsalt, Wtemp, Wflas)
+DAT <- mutate(.data=DAT, Rep=factor(Replicate), 
+              Transfer=as.numeric(Transfer), 
+              Treatment=factor(Treatment),
+              Temperature=factor(Temperature),
+              Media=factor(Media),
+              Experiment=factor(Experiment))
 
 shinyServer(function(input, output, session) {
-
+  
   # read the experiment data and subset based on input
   # clunky still
-  whatToPlot <- reactive({
-    data <- read.csv(input$Experiment, header=T, sep=',', quote='"')
-    data <- mutate(data, Rep=factor(Replicate), Transfer=as.numeric(Transfer), Treatment=factor(Treatment))
-    
-    # subset by Strain based on the group check box input
-    df <- data[data$Strain==input$Strain[1] |
-                     data$Strain==input$Strain[2] |
-                     data$Strain==input$Strain[3] |
-                     data$Strain==input$Strain[4] |
-                     data$Strain==input$Strain[5] ,]
-    
-    df <- df[df$Treatment==input$Treatment[1] |
-               df$Treatment==input$Treatment[2] |
-               df$Treatment==input$Treatment[3] |
-               df$Treatment==input$Treatment[4] |
-               df$Treatment==input$Treatment[5] ,]
+  whichExperiment <- reactive({
+    # subset by Experiment based on select input
+    DF <- switch(input$Experiment,
+                  "1" = droplevels.data.frame(DAT[which(DAT$Experiment=="salinity"), ]),
+                  "2" = droplevels.data.frame(DAT[which(DAT$Experiment=="temperature"), ]),
+                  "3" = droplevels.data.frame(DAT[which(DAT$Experiment=="flask"), ])
+    )
+    return(DF)
+  })
   
-    # subset by day based on inputs from the Day slider
-#    df <- df[which(df$Day >= input$Day[1] & df$Day <= input$Day[2]),]
-    
-    # subset by transfer (sequential replicate) based on inputs from the Transfer slider
-    df <- df[which(df$Transfer >= input$Transfer[1] & df$Transfer <= input$Transfer[2]),]
-
-    return(df) 
+  whichSubset <- reactive({
+    DF <- whichExperiment()
+    DF <- switch(input$Experiment,
+                  "1" = DF[which(DF$Strain %in% input$Strain1 & DF$Treatment %in% input$Treatment1
+                                   & DF$Transfer >= input$Transfer1[1] & DF$Transfer <= input$Transfer1[2]), ],
+                  "2" = DF[which(DF$Strain %in% input$Strain2 & DF$Treatment %in% input$Treatment2
+                                   & DF$Transfer >= input$Transfer2[1] & DF$Transfer <= input$Transfer2[2]), ],
+                  "3" = DF[which(DF$Strain %in% input$Strain3 & DF$Treatment %in% input$Treatment3
+                                   & DF$Transfer >= input$Transfer3[1] & DF$Transfer <= input$Transfer3[2]), ]
+                  )
+    return(DF) 
   })
   
   # plot the entire data frame as RF by Day
   pl1 <- reactive ({ 
-    plGrowCurv <- suppressWarnings(
-                  ggplot(data=whatToPlot(),
+    DF <- whichSubset()
+    ENV <- environment()
+    plGrowCurv <- ggplot(data=DF, environment = ENV,
                          aes(x=Day, y=RF-Rfctrl, ymax=max(RF)*1.05,
                          group=Rep, shape=Rep, 
                          colour=Rep, linetype=Rep)) +
@@ -47,11 +65,8 @@ shinyServer(function(input, output, session) {
                     facet_grid(Strain ~ Treatment) +
                     theme_bw() +
                     scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
-                                  labels = trans_format("log10", math_format(10^.x))) #+
-                  #  coord_fixed() #+
-  )
+                                  labels = trans_format("log10", math_format(10^.x))) 
   return(plGrowCurv)
-  
   })
   
   output$Plot <- renderPlot({  
--- ui.R
+++ ui.R
@@ -2,43 +2,97 @@ library(shiny)
 library(ggplot2)
 library(shinythemes)
 
-#get a named list of the active experiments 
-EXPS <- as.list(dir(pattern="MAIN.csv"))
-names(EXPS) <- EXPS  # to be used in selectInput("Experiment"...) bellow
-data <- read.csv(as.character(EXPS), header=TRUE)
-data <- mutate(data, Rep=factor(Replicate), Transfer=as.numeric(Transfer), Treatment=factor(Treatment))
+# get a named list of the active experiments 
+# not used
+#EXPS <- as.list(dir(pattern="*.csv"))
+#names(EXPS) <- EXPS  # to be used in selectInput("Experiment"...) bellow
 
-shinyUI(fluidPage(theme = shinytheme("flatly"),
-  
-  title="View growth curves and calculate growth rate",
-  plotOutput('Plot', height = 750), #width = 1400
-  hr(),
-  
-  fluidRow(
-    column(2,
-      selectInput("Experiment", 
-                  label = h3("Experiment"),
-                  choices = EXPS, selected = EXPS[1] ) ),
-    column(2,
-           checkboxGroupInput("Strain", 
-                       label = h3("Strain"),
-                       choices = levels(data$Strain), selected = levels(data$Strain)[1:5]) ),
-    column(2,
-           checkboxGroupInput("Treatment", 
-                              label = h3("Treatment"),
-                              choices = levels(data$Treatment), selected = levels(data$Treatment)[1:5]) ),
-  #  column(3,
-  #         sliderInput("Day", label = h3("Day Range"), 
-  #                     min = min(data$Day), 
-  #                     max = max(data$Day), 
-  #                     value = c(min(data$Day), max(data$Day)),
-  #                     ticks=TRUE, animate=TRUE)),
-   column(3,
-          sliderInput("Transfer", label = h3("Transfer Range"), 
-                      min = min(data$Transfer), 
-                      max = max(data$Transfer), 
-                      value = c(min(data$Transfer), max(data$Transfer)),
-                      ticks=TRUE, animate=TRUE))
-  )
+# update the datasets 
+# creates symbolic links from original files in different folder
+# will work only localy
+system("./update_datasets.sh")
 
-  ))
+# read all experiments: waller_salinity, waller_temperature, waller_flasks, cryptica_salinity
+Wsalt <- read.csv("./WSALT.csv", header=TRUE)
+Wtemp <- read.csv("./WTEMP.csv", header=TRUE)
+Wflas <- read.csv("./WFLAS.csv", header=TRUE)
+#Csalt <- read.csv("./CSALT.csv", header=TRUE)
+
+DAT <- rbind(Wsalt, Wtemp, Wflas)
+DAT <- mutate(.data=DAT, Rep=factor(Replicate), 
+                        Transfer=as.numeric(Transfer), 
+                        Treatment=factor(Treatment),
+                        Temperature=factor(Temperature),
+                        Media=factor(Media),
+                        Experiment=factor(Experiment))
+
+shinyUI(fluidPage(#theme = shinytheme("flatly"),
+  
+  titlePanel(h3("Salinity experiments")),
+  
+  tabPanel("Growth curves",
+           sidebarLayout(
+             sidebarPanel(width = 2,
+               radioButtons("Experiment", 
+                           label = h4("Experiment"),
+                           choices = list("salinity" = 1, "temperature" = 2, "flask" = 3), 
+                           selected = 1 ),
+             
+             conditionalPanel(condition= "input.Experiment == 1",
+                                     checkboxGroupInput("Strain1", 
+                                                        label = h4("Strain"),
+                                                        choices = levels(droplevels(DAT[DAT$Experiment=="salinity",]$Strain)), 
+                                                        selected = levels(droplevels(DAT[DAT$Experiment=="salinity",]$Strain))[1:5]),
+                                     checkboxGroupInput("Treatment1", 
+                                                        label = h4("Treatment"),
+                                                        choices = levels(droplevels(DAT[DAT$Experiment=="salinity",]$Treatment)), 
+                                                        selected = levels(droplevels(DAT[DAT$Experiment=="salinity",]$Treatment))[1:5]),
+                                     sliderInput("Transfer1", 
+                                                 label = h4("Transfer Range"), 
+                                                 min = min(DAT[DAT$Experiment=="salinity",]$Transfer), 
+                                                 max = max(DAT[DAT$Experiment=="salinity",]$Transfer), 
+                                                 value = c(max(DAT[DAT$Experiment=="salinity",]$Transfer)-4, 
+                                                           max(DAT[DAT$Experiment=="salinity",]$Transfer)), ticks=TRUE)
+                ),
+             
+             conditionalPanel(condition= "input.Experiment == 2",
+                                     checkboxGroupInput("Strain2", 
+                                                        label = h4("Strain"),
+                                                        choices = levels(droplevels(DAT[DAT$Experiment=="temperature",]$Strain)), 
+                                                        selected = levels(droplevels(DAT[DAT$Experiment=="temperature",]$Strain))[1:2]),
+                                     checkboxGroupInput("Treatment2", 
+                                                        label = h4("Treatment"),
+                                                        choices = levels(droplevels(DAT[DAT$Experiment=="temperature",]$Treatment)), 
+                                                        selected = levels(droplevels(DAT[DAT$Experiment=="temperature",]$Treatment))[1:4]),
+                                     sliderInput("Transfer2", 
+                                                 label = h4("Transfer Range"), 
+                                                 min = min(DAT[DAT$Experiment=="temperature",]$Transfer), 
+                                                 max = max(DAT[DAT$Experiment=="temperature",]$Transfer), 
+                                                 value = c(min(DAT[DAT$Experiment=="temperature",]$Transfer), 
+                                                           max(DAT[DAT$Experiment=="temperature",]$Transfer)), ticks=TRUE)
+                ),
+             
+             conditionalPanel(condition= "input.Experiment == 3",
+                              checkboxGroupInput("Strain3", 
+                                                 label = h4("Strain"),
+                                                 choices = levels(droplevels(DAT[DAT$Experiment=="flask",]$Strain)), 
+                                                 selected = levels(droplevels(DAT[DAT$Experiment=="flask",]$Strain))[1:5]),
+                              checkboxGroupInput("Treatment3", 
+                                                 label = h4("Treatment"),
+                                                 choices = levels(droplevels(DAT[DAT$Experiment=="flask",]$Treatment)), 
+                                                 selected = levels(droplevels(DAT[DAT$Experiment=="flask",]$Treatment))[1:4]),
+                              sliderInput("Transfer3", 
+                                          label = h4("Transfer Range"), 
+                                          min = min(DAT[DAT$Experiment=="flask",]$Transfer), 
+                                          max = max(DAT[DAT$Experiment=="flask",]$Transfer), 
+                                          value = c(min(DAT[DAT$Experiment=="flask",]$Transfer), 
+                                                    max(DAT[DAT$Experiment=="flask",]$Transfer)), ticks=TRUE)
+                )
+              ), # end sidePanel 
+             
+             mainPanel(width = 10,
+                      plotOutput('Plot', height = 950)
+             ) # end mainPanel
+  )) # end first tab
+
+  )) # end shinyUI and fluidPage
