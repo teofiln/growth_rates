04f21d39bbdfac313f1c8e8b2060ae99aebe1128
teo <teofiln@gmail.com>
2015-01-28 17:27:47 -0600

    new data and minor code changes

--- update_datasets.sh
+++ update_datasets.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-rm W****.csv
+rm *.csv
 
 ln -s ../Cwaller_2015-01-15/raw_data/TEMP.csv WTEMP.csv
 ln -s ../Cwaller_2014-11-22/raw_data/MAIN.csv WSALT.csv
 ln -s ../Cwaller_2014-11-22/raw_data/FLASKS.csv WFLAS.csv
 ln -s ../Cwaller_2014-11-22/raw_data/FAMI.csv WFAMI.csv
+ln -s ../Cryptica_2015-01-21/raw_data/CSALT.csv CSALT.csv
