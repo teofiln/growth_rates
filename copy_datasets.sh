a092abfde16a64ed7f4be55d0c1094fa59b85f82
teo <teofiln@gmail.com>
2015-01-30 17:51:55 -0600

    hard copied the data sets for some tests

---
+++ copy_datasets.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+rm *.csv
+
+cp ../Cwaller_2015-01-15/raw_data/TEMP.csv WTEMP.csv
+cp ../Cwaller_2014-11-22/raw_data/MAIN.csv WSALT.csv
+cp ../Cwaller_2014-11-22/raw_data/FLASKS.csv WFLAS.csv
+cp ../Cwaller_2014-11-22/raw_data/FAMI.csv WFAMI.csv
+cp ../Cryptica_2015-01-21/raw_data/CSALT.csv CSALT.csv
