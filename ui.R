e1a1218a4410b34e860eaa32bee793036baca605
Teofil <teofiln@gmail.com>
2015-02-28 12:09:13 -0600

    New tab for browsing data, uploads, transfers.
    data table for new uploads is not rendering.

--- ui.R
+++ ui.R
@@ -17,64 +17,83 @@ shinyUI(fluidPage(theme = shinytheme("flatly"),
   
   titlePanel(h4("Cyclotella salinity experiments")),
   tabsetPanel(id = "tabsets",
-              tabPanel("Data", value = "tab0",
-                       sidebarLayout(fluid = TRUE,
-                                     sidebarPanel(width = 2.5,
-                                                  fluidRow( column(width=3,
-                                                  radioButtons("Experiment0",
-                                                               label = h5("Experiment"),
-                                                               choices = list("C. wallercreekii tubes salinity" = 1, 
-                                                                              "C. wallercreekii tubes temperature" = 2, 
-                                                                              "C. cryptica tubes salinity" = 3, 
-                                                                              "C. wallercreekii flasks salinity" = 4, 
-                                                                              "C. wallercreekii flasks 2 salinity" = 5,
-                                                                              "C. cryptica flasks salinity" = 6),
-                                                               selected = 1)),
-                                                  column( width = 3,
-                                                          HTML("<button type='button' class='btn btn-info' data-toggle='collapse' data-target='#newData'>Upload new data</button>"),
-                                                          div(id = "newData", class = "collapse",
-                                                              wellPanel(
-                                                                actionButton("uploadNewData",
-                                                                             label="Upload tab delimited file"),
-                                                                hr(),
-                                                                actionButton("displayNewData",
-                                                                             label="Display new data"),
-                                                                hr(),
-                                                                actionButton("submitNewData",
-                                                                             label="Submit new data")
-                                                                ) # end wellPanel
-                                                              ) # end collapsable div
-                                                          ),
-                                                  
-                                                  column( width= 3,
-                                                  HTML("<button type='button' class='btn btn-info' data-toggle='collapse' data-target='#Transfer'>Prepare transfer</button>"),
-                                                  div(id = "Transfer", class = "collapse",
-                                                      wellPanel(
-                                                        numericInput("finalVolume", 
-                                                                     label=h5("Final volume (mL) in new transfer vessel"),
-                                                                     value=4),
-                                                        numericInput("desiredRFU", 
-                                                                     label=h5("Desired fluorescence (RFU/mL) at start of next transfer"),
-                                                                     value=500),
-                                                        actionButton("calculateTransferVolumes",
-                                                                     label="Calculate transfer volumes"),
-                                                        downloadButton("downloadTransferSheet",
-                                                                       label="Download")#,
+    tabPanel("Data", value = "tab0",
+      sidebarLayout(fluid = TRUE,
+        sidebarPanel(width = 3,
+          radioButtons("Experiment0",
+            label = h5("Experiment"),
+              choices = list("C. wallercreekii tubes salinity" = 1, 
+                "C. wallercreekii tubes temperature" = 2, 
+                "C. cryptica tubes salinity" = 3, 
+                "C. wallercreekii flasks salinity" = 4, 
+                "C. wallercreekii flasks 2 salinity" = 5,
+                "C. cryptica flasks salinity" = 6),
+                selected = 1),
+        wellPanel(
+          HTML("<button type='button' class='btn btn-info' data-toggle='collapse' data-target='#newData'>Upload new data</button>"),
+          div(id = "newData", class = "collapse",
+            checkboxInput('header', 'Header', FALSE),
+            numericInput('skip_lines', label="How many lines to skip", value=0),
+            radioButtons('sep', 'Separator',
+              c(Comma=',',
+              Semicolon=';',
+              Tab='\t'),
+              selected='\t'),
+            radioButtons('quote', 'Quote',
+              c(None='',
+              'Double Quote'='"',
+              'Single Quote'="'"),
+              selected='"'),
+            numericInput("numTreat", "Number of treatments", value = 5),
+            numericInput("numStrain", "Number of strains", value = 5),
+            tags$hr(),
+            fileInput('file1', 'Choose file to upload',
+                      accept = c(
+                        'text/csv',
+                        'text/comma-separated-values',
+                        'text/tab-separated-values',
+                        'text/plain',
+                        '.csv',
+                        '.tsv')
+            ),
+            tags$hr(),
+            actionButton("submitNewData",
+              label="Submit new data")
+            ) # end collapsable div
+          ), # end wellPanel
+        wellPanel(
+          HTML("<button type='button' class='btn btn-info' data-toggle='collapse' data-target='#Transfer'>Prepare transfer</button>"),
+          tags$hr(),
+          div(id = "Transfer", class = "collapse",
+          numericInput("finalVolume", min=1, max=1000,
+            label="Final volume (mL) in new transfer vessel", value=4),
+          numericInput("desiredRFU",
+            label="Desired fluorescence (RFU/mL) at start of next transfer", value=500),
+          downloadButton("downloadTransferSheet", label="Download")
+          ) # end collapsible div
+        ) # end wellPanel
+      ), # end sidebarPanel
+        
+          
 #                                                 hr(),
 #                                                 strong("Caution: Clicking 'Submit Transfer' will add a day's worth of measurements to the dataset. 
 #                                                            Use only when Transfer is actually performed!"),
 #                                                 hr(),
 #                                                 actionButton("submitTransfer", label="Submit Transfer")
-                                                      ) # end wellPanel
 
-                                            )))#, # end collapsable div
-
-                                     ), # end sidebarPanel
-                                     mainPanel(width = 9.5,
-                                       dataTableOutput("Table0")
-                                       ) # end main panel
-                       ) # end sidebarPanel
-              ), # end tab0
+          mainPanel(width = 9,
+            HTML("<button type='button' class='btn btn-info' data-toggle='collapse' data-target='#allData'>All data</button>"),
+            div(id = "allData", class = "collapse in",
+              dataTableOutput("Table0a")),
+            HTML("<button type='button' class='btn btn-info' data-toggle='collapse' data-target='#newUpload'>Newly uploaded</button>"),
+            div(id = "newUpload", class = "collapse",
+              dataTableOutput("Table0b")),
+            HTML("<button type='button' class='btn btn-info' data-toggle='collapse' data-target='#transferData'>Transfer sheet</button>"),
+            div(id = "transferData", class = "collapse",
+              dataTableOutput("Table0c"))
+            ) # end main panel
+        ) # end sidebarLayout
+    ), # end tab0
                                                   
               tabPanel("Growth curves", value = "tab1",
                        sidebarLayout(fluid = TRUE,
@@ -99,7 +118,7 @@ shinyUI(fluidPage(theme = shinytheme("flatly"),
             
             tabPanel("Compare slopes", value = "tab2",
                      sidebarLayout(
-                       sidebarPanel(width = 2,
+                       sidebarPanel(width = 3,
                                     radioButtons("Experiment2",
                                                  label = h5("Experiment"),
                                                  choices = list("C. wallercreekii tubes salinity" = 1, 
@@ -109,7 +128,7 @@ shinyUI(fluidPage(theme = shinytheme("flatly"),
                                     HTML("<button type='button' class='btn btn-info' data-toggle='collapse' data-target='#PlotControls2'>Customize</button>"),
                                     uiOutput("whichSelectInput")
                                     ),
-                       mainPanel(width = 10,
+                       mainPanel(width = 9,
                                  plotOutput('Plot2', height = 500),
                                  tableOutput('TableAOV')
                                  ) # end mainPanel
