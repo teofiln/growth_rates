d95216252cea0d25a28343882b28d46b7fdeeacd
teo <teofiln@gmail.com>
2015-03-02 18:06:21 -0600

    removed scripts for updating the data as this is now done through the app's gui

--- update_datasets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm *.csv
-
-ln -s ../Cwaller_2015-01-15/raw_data/TEMP.csv WTEMP.csv
-ln -s ../Cwaller_2014-11-22/raw_data/MAIN.csv WSALT.csv
-ln -s ../Cwaller_2014-11-22/raw_data/FLASKS.csv WFLAS.csv
-ln -s ../Cwaller_2014-11-22/raw_data/FAMI.csv WFAMI.csv
-ln -s ../Cryptica_2015-01-21/raw_data/CSALT.csv CSALT.csv
